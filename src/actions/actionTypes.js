acc99b9e37eeba42cae8c016c7f19cc695c4adf2
Hance <Joseph.Hance@salliemae.com>
2021-01-27 11:29:19 -0500

    Using Flux

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,6 +1,8 @@
-export default {
+const actionTypes = {
   CREATE_COURSE: "CREATE_COURSE",
   UPDATE_COURSE: "UPDATE_COURSE",
   DELETE_COURSE: "DELETE_COURSE",
   LOAD_COURSES: "LOAD_COURSES"
 };
+
+export default actionTypes;
